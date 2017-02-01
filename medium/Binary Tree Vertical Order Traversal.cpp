d55becfe43c8ca5972df14fef7e52ed97985d9ae
Isaiah Paradiso <isparadi@microsoft.com>
2017-02-01 15:12:40 -0800

    update

--- medium/Binary Tree Vertical Order Traversal.cpp
+++ medium/Binary Tree Vertical Order Traversal.cpp
@@ -1,23 +1,67 @@
-/*144. Binary Tree Preorder Traversal   QuestionEditorial Solution  My Submissions
-Total Accepted: 146794
-Total Submissions: 347920
+/*314. Binary Tree Vertical Order Traversal   Add to List QuestionEditorial Solution  My Submissions
+Total Accepted: 19550
+Total Submissions: 54803
 Difficulty: Medium
 Contributors: Admin
-Given a binary tree, return the preorder traversal of its nodes' values.
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
 
-For example:
-Given binary tree {1,#,2,3},
-   1
-    \
-     2
-    /
+If two nodes are in the same row and column, the order should be from left to right.
+
+Examples:
+
+Given binary tree [3,9,20,null,null,15,7],
    3
-return [1,2,3].
-
-Note: Recursive solution is trivial, could you do it iteratively?
-
-Hide Tags Tree Stack
-Hide Similar Problems (M) Binary Tree Inorder Traversal (M) Verify Preorder Sequence in Binary Search Tree
+  /\
+ /  \
+ 9  20
+    /\
+   /  \
+  15   7
+return its vertical order traversal as:
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+Given binary tree [3,9,8,4,0,1,7],
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+return its vertical order traversal as:
+[
+  [4],
+  [9],
+  [3,0,1],
+  [8],
+  [7]
+]
+Given binary tree [3,9,8,4,0,1,7,null,null,null,2,5] (0's right child is 2 and 1's left child is 5),
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+    /\
+   /  \
+   5   2
+return its vertical order traversal as:
+[
+  [4],
+  [9,5],
+  [3,0,1],
+  [8,2],
+  [7]
+]
+Hide Company Tags Google Snapchat Facebook
+Hide Tags Hash Table
+Hide Similar Problems (M) Binary Tree Level Order Traversal
 
 */
 
@@ -32,43 +76,44 @@ Hide Similar Problems (M) Binary Tree Inorder Traversal (M) Verify Preorder Sequ
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> result;
-        stack<TreeNode*> s;
-        while (root || !s.empty()) {
-            while (root) {
-                result.push_back(root->val);
-                s.push(root);
-                root = root->left;
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        
+        map<int, vector<int>> levels;
+        vector<vector<int>> result;
+        if (!root)
+        {
+            return result;
+        }
+        
+        queue<pair<int, TreeNode*>> q;
+        q.push({0, root});
+        while (!q.empty())
+        {
+            auto p = q.front();
+            q.pop();
+            
+            int verticalLevel = p.first;
+            TreeNode* node = p.second;
+            
+            auto& list = levels[verticalLevel];
+            list.push_back(node->val);
+            
+            if (node->left)
+            {
+                q.push({verticalLevel-1, node->left});
             }
-
-            if(!s.empty()){
-                auto node = s.top();
-                s.pop();
-                root = node->right;
+            if (node->right)
+            {
+                q.push({verticalLevel+1, node->right});
             }
+        }
+        
+        for (auto& iter : levels)
+        {
+            result.push_back(iter.second);
         }
         return result;
     }
     
-    // another one that's easier
-    vector<int> preorderTraversal(TreeNode* root) {
-        
-        vector<int> res;
-        if (!root) return res;
-        stack<TreeNode*> s;
-        s.push(root);
-        
-        while(!s.empty())
-        {
-            auto n = s.top();
-            s.pop();
-            res.push_back(n->val);
-            
-            if (n->right) s.push(n->right);
-            if (n->left) s.push(n->left);
-        }
-        return res;
-        
-    }
+
 };
