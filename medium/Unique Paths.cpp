65d21dda9c72cb8ee28b5af05f463875f28bdc37
Isaiah Paradiso <isaiah.paradiso@gmail.com>
2017-02-08 20:47:36 -0800

    update

---
+++ medium/Unique Paths.cpp
@@ -0,0 +1,35 @@
+/*
+ A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ 
+ The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ 
+ How many possible unique paths are there?
+ 
+ 
+ Above is a 3 x 7 grid. How many possible unique paths are there?
+ 
+ Note: m and n will be at most 100.
+ 
+ Hide Company Tags Bloomberg
+ Hide Tags Array Dynamic Programming
+ Hide Similar Problems (M) Unique Paths II (M) Minimum Path Sum (H) Dungeon Game
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>> memo(m, vector<int>(n, 1));
+        
+        for (int i=1; i<m; i++)
+        {
+            for (int j=1; j<n; ++j)
+            {
+                memo[i][j] = memo[i-1][j] + memo[i][j-1];
+            }
+        }
+        
+        return memo[m-1][n-1];
+        
+    }
+};
